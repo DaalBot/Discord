479c4e179a98863b1083b4dc59fe861d163ca933
pinyalt <>
2024-01-02 13:05:22 +0000

    Update .gitignore and add config.example.jsonc and status.js

--- index.js
+++ index.js
@@ -6,26 +6,13 @@ const config = require('./config.json');
 require('./launch-extra.js');
 client.setMaxListeners(0); // Sets max client listeners to infinity
 const fs = require('fs'); // File system
-const { EmbedBuilder, ActivityType, ApplicationCommandType } = require('discord.js'); // Embeds
 
 // Functions
 function OnReady() {
-  const loggingChannel = client.channels.cache.find(channel => channel.id === '1138413163319132231')
   console.log('Load > DaalBot is ready')
 }
 
 client.on('ready', () => {
-  //When bot loads
-  console.log(`Running in ${client.guilds.cache.size} servers!`);
-  const RandomIndex = Math.floor(Math.random() * (config.activities.length - 1) + 1);
-  const NewActivity = config.activities[RandomIndex];
-
-    client.user.setActivity(NewActivity, {
-      type: ActivityType.Custom
-    });
-    console.log(`Status > Status is now "${NewActivity}"`)
-  
-  // WOKCommands
   new WOKCommands({
     client,
     commandsDir: path.join(__dirname, 'commands'),
